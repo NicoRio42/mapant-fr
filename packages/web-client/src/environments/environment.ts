2a350f33a5326e319a084e6909121e1e484275b6
Nicolas Rio <nicolas.rio42@gmail.com>
2023-04-17 08:52:24 +0200

    Typo

--- packages/web-client/src/environments/environment.ts
+++ packages/web-client/src/environments/environment.ts
@@ -1,2 +1,2 @@
 // export const TILES_BASE_URL = 'http://127.0.0.1:5000/';
-export const TILES_BASE_URL = 'https://www.mapant-tiles.ovh//tiles/';
+export const TILES_BASE_URL = 'https://www.mapant-tiles.ovh/tiles/';
